faa42d1be0a7dc419091a2112f2a9d51391b9894
flgisimone <florestagiuliosimone@gmail.com>
2022-10-24 16:45:02 +0200

    esercitazione 24-10-22

--- 24-10-22/script.js
+++ 24-10-22/script.js
@@ -1,7 +1,9 @@
 function wishlist(){
     alert("Aggiunto alla lista desideri");
+    console.log("Aggiunto alla lista desideri");
 }
 
 function waitingList(){
     alert("Aggiunto alla lista d'attesa");
+    console.log("Aggiunto alla lista d'attesa");
 }
